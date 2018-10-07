c211909df2e2772bd1ca6a47c18eb2c28f212fc7
Antonio Juliano <antonio.m.juliano@gmail.com>
2018-10-06 20:01:17 -0700

    work on server

--- server/helpers/constants.js
+++ server/helpers/constants.js
@@ -1,2 +1,3 @@
 export const EXECUTER_INTERVAL_MS = 10000;
 export const ACCOUNT = '0x';
+export const ETH_NODE_URL = 'ws://localhost:8546';
--- server/lib/conditional.js
+++ server/lib/conditional.js
@@ -1,8 +1,9 @@
 import Web3 from 'web3';
 import Conditional from '../artifacts/Conditional.json';
+import { ETH_NODE_URL } from '../helpers/constants';
 
-const web3 = new Web3('https://kovan.infura.io/v3/828475865dfe4c31a9c8d6321250f7e0');
+const web3 = new Web3(Web3.providers.WebsocketProvider(ETH_NODE_URL));
 
-const conditional = new web3.eth.Contract(Conditional.abi, conditional.networks[42].address);
+const conditional = new web3.eth.Contract(Conditional.abi, Conditional.networks['42'].address);
 
 export default conditional;
